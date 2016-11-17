3144e6778a2d6312637fdbf93e78384878933e8d
Andres Kwan <andres.x.kwan.-nd@disney.com>
2016-11-17 15:21:28 -0500

    [C.E.S] - [Enum] - [Custom Initializers]

--- ShapesWithClassesEnumsStructs.playground/Contents.swift
+++ ShapesWithClassesEnumsStructs.playground/Contents.swift
@@ -14,7 +14,9 @@ enum CSSColor {
     case rgb(UInt8, UInt8, UInt8)
 }
 
+//Protocols and Methods with an Enum
 extension CSSColor: CustomStringConvertible {
+//You’re required to implement a getter for a description string property.
     var description: String {
         switch self {
         case .named(let colorName):
@@ -24,9 +26,20 @@ extension CSSColor: CustomStringConvertible {
         }
     }
 }
-
 let fill = CSSColor.named(.fuchsia)
 let fillRGB = CSSColor.rgb(100, 100, 100)
 
 print(fillRGB)
 
+//Initializers with an Enum
+//make a custom initializer for grayscale values.
+extension CSSColor {
+    init(gray: UInt8) {
+        self = .rgb(gray, gray, gray)
+    }
+}
+
+let color3 = CSSColor(gray: 0xaa)
+print(color3)
+
+//Namespaces with Enum
