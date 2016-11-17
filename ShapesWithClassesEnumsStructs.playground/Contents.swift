c55ec7ed7d84fdb448d9f00adceaa9ccfb213525
Andres Kwan <andres.x.kwan.-nd@disney.com>
2016-11-17 11:56:02 -0500

    [Classes, Enums, Structs] - [Enum] - [Associative types] - [One Enum representing many kinds of data/options/representations]

--- ShapesWithClassesEnumsStructs.playground/Contents.swift
+++ ShapesWithClassesEnumsStructs.playground/Contents.swift
@@ -8,3 +8,8 @@ enum ColorName: String {
     case black, silver, gray, white, maroon, red, purple, fuchsia, green, lime, olive, yellow, navy, blue, teal, aqua
 }
 
+//Associated Values
+enum CSSColor {
+    case named(ColorName)
+    case rgb(UInt8, UInt8, UInt8)
+}
