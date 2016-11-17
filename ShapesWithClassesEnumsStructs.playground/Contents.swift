04c4cd0eca2f379a2bd9c91ece6b5ff653054ca2
Andres Kwan <andres.x.kwan.-nd@disney.com>
2016-11-17 11:52:09 -0500

    [Classes, Enums, Structs] - [Enum] - [Protocol RawRepresentable as String]

---
+++ ShapesWithClassesEnumsStructs.playground/Contents.swift
@@ -0,0 +1,10 @@
+/*Goal Create framework for SVG
+ render a svg file*/
+import Foundation
+
+//CSS colors
+// Enum String as a RawRepresentable
+enum ColorName: String {
+    case black, silver, gray, white, maroon, red, purple, fuchsia, green, lime, olive, yellow, navy, blue, teal, aqua
+}
+
