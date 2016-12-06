3cefc98c47e2c3d575bf8b921865b29fcc51373f
Andres Kwan <andres.x.kwan.-nd@disney.com>
2016-12-06 14:23:23 -0500

    [408 - PG] - [update CoreGraphics] - [swift 3.0] - [CoreGraphicsDiagramView]

--- Crustacean.playground/Sources/CoreGraphicsDiagramView.swift
+++ Crustacean.playground/Sources/CoreGraphicsDiagramView.swift
@@ -1,5 +1,5 @@
 import UIKit
-import XCPlayground
+import PlaygroundSupport
 
 let drawingArea = CGRect(x: 0.0, y: 0.0, width: 375.0, height: 667.0)
 
@@ -7,17 +7,17 @@ let drawingArea = CGRect(x: 0.0, y: 0.0, width: 375.0, height: 667.0)
 /// user-supplied function to generate paths in a `CGContext`, then strokes
 /// the context's current path, creating lines in a pleasing shade of blue.
 class CoreGraphicsDiagramView : UIView {
-    override func drawRect(rect: CGRect) {
+    override func draw(_ rect: CGRect) {
         let context = UIGraphicsGetCurrentContext()!
-        CGContextSaveGState(context)
+        context.saveGState()
         draw(context)
         
-        let lightBlue = CGColorCreate(
-            CGColorSpaceCreateDeviceRGB(), [0.222, 0.617, 0.976, 1.0])
-        CGContextSetStrokeColorWithColor(context, lightBlue)
-        CGContextSetLineWidth(context, 3)
-        CGContextStrokePath(context)
-        CGContextRestoreGState(context)
+        let lightBlue = CGColor(
+            colorSpace: CGColorSpaceCreateDeviceRGB(), components: [0.222, 0.617, 0.976, 1.0])
+        context.setStrokeColor(lightBlue!)
+        context.setLineWidth(3)
+        context.strokePath()
+        context.restoreGState()
     }
     
     var draw: (CGContext)->() = { _ in () }
@@ -26,10 +26,10 @@ class CoreGraphicsDiagramView : UIView {
 /// Shows a `UIView` in the current playground that draws itself by invoking
 /// `draw` on a `CGContext`, then stroking the context's current path in a
 /// pleasing light blue.
-public func showCoreGraphicsDiagram(title: String, draw: (CGContext)->()) {
+public func showCoreGraphicsDiagram(title: String, draw: @escaping (CGContext)->()) {
     let diagramView = CoreGraphicsDiagramView(frame: drawingArea)
     diagramView.draw = draw
     diagramView.setNeedsDisplay()
-    XCPlaygroundPage.currentPage.liveView = diagramView
+    PlaygroundPage.current.liveView = diagramView
 }
 
